5d0dea2da251de2e4f394c18cbc6b0fe8a767f65
Shwetha Marumada <shwe.nie@gmail.com>
2025-05-08 04:00:31 +0100

    Refactor the code and Styled using CSS Bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,13 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    set_task
   end
 
   def new
@@ -18,17 +21,20 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    set_task
   end
 
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    set_task
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    set_task
     @task.destroy
     # No need for app/views/tasks/destroy.html.erb
     redirect_to tasks_path, status: :see_other
@@ -38,5 +44,11 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
